eeb643f04dd0508081bb5e8b8bed410b8d736082
horameus <ludo.hartmann@gmail.com>
2025-03-12 02:27:55 +0100

    utilities: imported String and ClassName features from digitalnet

---
+++ src/utilities/String/index.ts
@@ -0,0 +1,3 @@
+export { default as StringIdentity } from './StringIdentity';
+export { default as StringMatcher } from './StringMatcher';
+export { default as StringResolver } from './StringResolver';

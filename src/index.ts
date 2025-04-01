fb4adf4d0f1995fe414e8dd3095bc68d4ee9dbf6
horameus <ludo.hartmann@gmail.com>
2025-04-01 22:07:41 +0200

    components: removed Test component

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { type Config } from '@measured/puck';
 import { categories } from './categories';
 import root from './root';
-import Test from './components/Test/Test';
 import { Text, Grid, GridItem } from './components';
 
 export default {
@@ -9,7 +8,6 @@ export default {
     categories,
     components: {
         //... Add components here
-        Test,
         Text,
         Grid,
         GridItem,
